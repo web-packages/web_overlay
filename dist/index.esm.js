46c18e1de00b7a2b4e294e41730d8074afe94984
Dev TtangKong <ttankkeo112@gmail.com>
2024-06-23 19:18:52 +0900

    Perform rollup foundation configuration

---
+++ dist/index.esm.js
@@ -0,0 +1,2 @@
+console.log(0);
+//# sourceMappingURL=index.esm.js.map
---
+++ dist/index.umd.js
@@ -0,0 +1,2 @@
+!function(n){"function"==typeof define&&define.amd?define(n):n()}((function(){"use strict";console.log(0)}));
+//# sourceMappingURL=index.umd.js.map
